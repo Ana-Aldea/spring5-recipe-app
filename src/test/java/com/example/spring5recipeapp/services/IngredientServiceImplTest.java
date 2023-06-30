08a512b38f626d0afdab2e44bd06f9b38db70b88
ana <ana.aldea.c@gmail.com>
2023-06-30 14:28:24 +0300

    Adding delete ingredient.

--- src/test/java/com/example/spring5recipeapp/services/IngredientServiceImplTest.java
+++ src/test/java/com/example/spring5recipeapp/services/IngredientServiceImplTest.java
@@ -6,8 +6,8 @@ import com.example.spring5recipeapp.converters.IngredientToIngredientCommand;
 import com.example.spring5recipeapp.converters.UnitOfMeasureCommandToUnitOfMeasure;
 import com.example.spring5recipeapp.converters.UnitOfMeasureToUnitOfMeasureCommand;
 import com.example.spring5recipeapp.domain.Ingredient;
-import com.example.spring5recipeapp.repositories.RecipeRepository;
 import com.example.spring5recipeapp.domain.Recipe;
+import com.example.spring5recipeapp.repositories.RecipeRepository;
 import com.example.spring5recipeapp.repositories.UnitOfMeasureRepository;
 import org.junit.Before;
 import org.junit.Test;
@@ -105,6 +105,26 @@ public class IngredientServiceImplTest {
         verify(recipeRepository, times(1)).findById(anyLong());
         verify(recipeRepository, times(1)).save(any(Recipe.class));
 
+    }
+
+    @Test
+    public void testDeleteById() throws Exception {
+        //given
+        Recipe recipe = new Recipe();
+        Ingredient ingredient = new Ingredient();
+        ingredient.setId(3L);
+        recipe.addIngredient(ingredient);
+        ingredient.setRecipe(recipe);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        //when
+        ingredientService.deleteById(1L, 3L);
+
+        //then
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, times(1)).save(any(Recipe.class));
     }
 
 }
